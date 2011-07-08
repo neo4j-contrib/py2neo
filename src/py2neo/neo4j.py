fb1a23b5393b54b025711489eaf2a1e7d4e7de90
Nigel Small <nigel@nigelsmall.name>
2011-07-08 21:05:51 +0100

    Added file name to top comment

--- src/py2neo/neo4j.py
+++ src/py2neo/neo4j.py
@@ -20,6 +20,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+---
+
+neo4j.py
+
 """
 
 import httplib2
