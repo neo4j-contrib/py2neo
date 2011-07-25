d715aef601ad717fd89dc0620c3be8132927fdb3
Nigel Small <nigel@nigelsmall.name>
2011-07-25 08:17:08 +0100

    More doc updates

--- src/py2neo/neo4j.py
+++ src/py2neo/neo4j.py
@@ -74,6 +74,7 @@ class GraphDatabaseService(rest.Resource):
 		properties, if supplied.
 		
 		@param properties: a dictionary of properties to attach to this C{Node} (optional)
+		@return: a C{Node} instance representing the newly created node
 		
 		"""
 		return Node(self._post(self._lookup('node'), properties), http=self._http)
@@ -82,6 +83,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Returns a C{Node} object representing the reference node for this
 		database instance.
+		
+		@return: a C{Node} instance representing the database reference node
+		
 		"""
 		return Node(self._lookup('reference_node'), http=self._http)
 
@@ -89,6 +93,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Returns a list of C{Relationship} names currently defined within this
 		database instance.
+		
+		@return: a list of C{Relationship} names
+		
 		"""
 		return self._get(self._lookup('relationship_types'))
 
@@ -96,6 +103,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Creates a new C{Node} C{Index} with the supplied name and
 		configuration.
+		
+		@return: an C{Index} instance representing the newly created index
+		
 		"""
 		return Index(Node, uri=self._post(self._lookup('node_index'), {
 			'name': name,
@@ -106,6 +116,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Returns a dictionary of all available C{Node} C{Index}es within this
 		database instance.
+		
+		@return: a dictionary of Name : C{Index} mappings for all C{Node} C{Index}es
+		
 		"""
 		indexes = self._get(self._lookup('node_index')) or {}
 		return dict([
@@ -117,6 +130,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Creates a new C{Relationship} C{Index} with the supplied name and
 		configuration.
+		
+		@return: an C{Index} instance representing the newly created index
+		
 		"""
 		return Index(Relationship, uri=self._post(self._lookup('relationship_index'), {
 			'name': name,
@@ -127,6 +143,9 @@ class GraphDatabaseService(rest.Resource):
 		"""
 		Returns a dictionary of all available C{Relationship} C{Index}es within
 		this database instance.
+		
+		@return: a dictionary of Name : C{Index} mappings for all C{Relationship} C{Index}es
+		
 		"""
 		indexes = self._get(self._lookup('relationship_index')) or {}
 		return dict([
@@ -135,6 +154,17 @@ class GraphDatabaseService(rest.Resource):
 		])
 
 	def execute(self, plugin_name, function_name, data):
+		"""
+		Executes a POST request against the specified plugin function using the
+		supplied data.
+		
+		@param plugin_name: the name of the plugin to call
+		@param function_name: the name of the function to call within in the specified plugin
+		@param data: the data to pass to the function call
+		@raise NotImplementedError: when the specfifed plugin or function is not available
+		@return: the data returned from the function call
+		
+		"""
 		if plugin_name not in self._extensions:
 			raise NotImplementedError(plugin_name)
 		plugin = self._extensions[plugin_name]
@@ -144,11 +174,27 @@ class GraphDatabaseService(rest.Resource):
 		return self._post(function_uri, data)
 
 	def execute_cypher_query(self, query):
+		"""
+		Executes the supplied query using the CypherPlugin, if available.
+		
+		@param query: a string containing the Cypher query to execute
+		@raise NotImplementedError: if the Cypher plugin is not available
+		@return: the result of the Cypher query
+		
+		"""
 		return self.execute('CypherPlugin', 'execute_query', {
 			'query': query
 		});
 
 	def execute_gremlin_script(self, script):
+		"""
+		Executes the supplied script using the GremlinPlugin, if available.
+		
+		@param script: a string containing the Gremlin script to execute
+		@raise NotImplementedError: if the Gremlin plugin is not available
+		@return: the result of the Gremlin script
+		
+		"""
 		return self.execute('GremlinPlugin', 'execute_script', {
 			'script': script
 		});
@@ -189,37 +235,58 @@ class IndexableResource(rest.Resource):
 
 	def __eq__(self, other):
 		"""
-		Determines equality of two objects based on both URI and C{Index}
-		entry URI, if available.
+		Determines equality of two resource representations based on both URI
+		and C{Index} entry URI, if available.
 		"""
 		return self._uri == other._uri and self._index_entry_uri == other._index_entry_uri
 
 	def __ne__(self, other):
 		"""
-		Determines inequality of two objects based on both URI and C{Index}
-		entry URI, if available.
+		Determines inequality of two resource representations based on both URI
+		and C{Index} entry URI, if available.
 		"""
 		return self._uri != other._uri or self._index_entry_uri != other._index_entry_uri
 
 	def __getitem__(self, key):
+		"""
+		Returns a named property for this resource.
+		"""
 		return self._get(self._lookup('property').format(key=key))
 
 	def __setitem__(self, key, value):
+		"""
+		Sets a named property for this resource to the supplied value.
+		"""
 		self._put(self._lookup('property').format(key=key), value)
 
 	def __delitem__(self, key):
+		"""
+		Deletes a named property for this resource.
+		"""
 		self._delete(self._lookup('property').format(key=key))
 
 	def get_properties(self):
+		"""
+		Returns all properties for this resource.
+		"""
 		return self._get(self._lookup('properties'))
 
 	def set_properties(self, properties):
+		"""
+		Sets all properties for this resource to the supplied values.
+		"""
 		self._put(self._lookup('properties'), properties)
 
 	def remove_properties(self):
+		"""
+		Deletes all properties for this resource.
+		"""
 		self._delete(self._lookup('properties'))
 
 	def delete(self):
+		"""
+		Deletes this resource from the database instance.
+		"""
 		self._delete(self._lookup('self'))
 
 
@@ -257,6 +324,17 @@ class Node(IndexableResource):
 		return "(%d)" % self._id
 
 	def create_relationship_to(self, other_node, type, data=None):
+		"""
+		Creates a new C{Relationship} of type C{type} from the C{Node}
+		represented by the current instance to the C{Node} represented by
+		C{other_node}.
+		
+		@param other_node: an end C{Node} for the new C{Relationship}
+		@param type: the type of the new C{Relationship}
+		@param data: the data to attach to the new C{Relationship} (optional)
+		@return: the newly created C{Relationship}
+		
+		"""
 		return Relationship(self._post(self._lookup('create_relationship'), {
 			'to': other_node._uri,
 			'type': type,
@@ -264,6 +342,15 @@ class Node(IndexableResource):
 		}), http=self._http)
 
 	def get_relationships(self, direction, *types):
+		"""
+		Returns all C{Relationship}s from the current C{Node} in a given
+		C{direction} of a specific C{type} (if supplied).
+		
+		@param direction: a string constant from the C{Direction} class
+		@param types: the types of C{Relationship}s to include (optional)
+		@return: a list of C{Relationship}s matching the specified criteria
+		
+		"""
 		if len(types) == 0:
 			uri = self._lookup(direction + '_relationships')
 		else:
@@ -274,6 +361,16 @@ class Node(IndexableResource):
 		]
 
 	def get_related_nodes(self, direction, *types):
+		"""
+		Returns all C{Node}s related to the current C{Node} by a
+		C{Relationship} in a given C{direction} of a specific C{type}
+		(if supplied).
+		
+		@param direction: a string constant from the C{Direction} class
+		@param types: the types of C{Relationship}s to include (optional)
+		@return: a list of C{Node}s matching the specified criteria
+		
+		"""
 		if len(types) == 0:
 			uri = self._lookup(direction + '_relationships')
 		else:
@@ -287,6 +384,12 @@ class Node(IndexableResource):
 		]
 
 	def get_traverser(self):
+		"""
+		Returns a C{Traverser} instance for the current C{Node}.
+		
+		@return: a C{Traverser} for this C{Node}
+		
+		"""
 		return Traverser(self._lookup('traverse').format(returnType='path'), http=self._http)
 
 
@@ -329,6 +432,9 @@ class Relationship(IndexableResource):
 		"""
 		Returns a C{Node} object representing the start node within this
 		C{Relationship}.
+		
+		@return: the start C{Node} of this C{Relationship}
+		
 		"""
 		return Node(self._lookup('start'), http=self._http)
 
@@ -336,6 +442,9 @@ class Relationship(IndexableResource):
 		"""
 		Returns a C{Node} object representing the end node within this
 		C{Relationship}.
+		
+		@return: the end C{Node} of this C{Relationship}
+		
 		"""
 		return Node(self._lookup('end'), http=self._http)
 
@@ -389,21 +498,58 @@ class Path(object):
 		]) + str(self._nodes[-1])
 
 	def __len__(self):
+		"""
+		Returns the length of this C{Path} (equivalent to the number of
+		C{Relationship}s).
+		"""
 		return len(self._relationships)
 
 	def get_nodes(self):
+		"""
+		Returns a list of all the C{Node}s which make up this C{Path}.
+		
+		@return: a list of C{Node}s for this C{Path}
+		
+		"""
 		return self._nodes
 
 	def get_relationships(self):
+		"""
+		Returns a list of all the C{Relationship}s which make up this C{Path}.
+		
+		@return: a list of C{Relationship}s for this C{Path}
+		
+		"""
 		return self._relationships
 
 	def get_start_node(self):
+		"""
+		Returns a C{Node} object representing the first node within this
+		C{Path}.
+		
+		@return: the first C{Node} in this C{Path}
+		
+		"""
 		return self._nodes[0]
 
 	def get_end_node(self):
+		"""
+		Returns a C{Node} object representing the last node within this
+		C{Path}.
+		
+		@return: the last C{Node} in this C{Path}
+		
+		"""
 		return self._nodes[-1]
 
 	def get_last_relationship(self):
+		"""
+		Returns a C{Relationship} object representing the last relationship
+		within this C{Path}.
+		
+		@return: the last C{Relationship} in this C{Path} or C{None} if zero length
+		
+		"""
 		return self._relationships[-1] if len(self._relationships) > 0 else None
 
 
