fd1ce980a7723035387298ae1a7a4312481db7dc
Nigel Small <nigel@nigelsmall.name>
2012-02-09 21:06:10 +0000

    updated version to 1.0.0

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
 	name="py2neo",
-	version="0.992",
+	version="1.0.0",
 	description="Python bindings to Neo4j",
 	long_description="""The py2neo project provides bindings between Python and Neo4j via its RESTful web service interface. It attempts to be both Pythonic and consistent with the core Neo4j API.""",
 	author="Nigel Small",
-	author_email="py2neo@nigelsmall.org",
+	author_email="py2neo@nigelsmall.net",
 	url="http://py2neo.org/",
 	package_dir={"": "src"},
 	packages=["py2neo"],
