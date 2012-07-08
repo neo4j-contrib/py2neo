6c6db2394e45534159dc384c880878307c373cfa
Nigel Small <nigel@nigelsmall.name>
2012-07-08 14:49:37 +0100

    added geoff script

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 	author="Nigel Small",
 	author_email="py2neo@nigelsmall.net",
 	url="http://py2neo.org/",
-	scripts=["scripts/cypher"],
+	scripts=["scripts/cypher", "scripts/geoff"],
 	package_dir={"": "src"},
 	packages=["py2neo"],
 	license="Apache License, Version 2.0",
