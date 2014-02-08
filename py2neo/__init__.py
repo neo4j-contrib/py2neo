84cdeef69d69048ec0f68b6de05d3b767bb8cd07
Nigel Small <nigel@nigelsmall.com>
2014-02-08 18:26:45 +0000

    fixed #251 - version

--- py2neo/__init__.py
+++ py2neo/__init__.py
@@ -21,7 +21,7 @@ __copyright__ = "2011-2014, Nigel Small"
 __email__ = "nigel@nigelsmall.com"
 __license__ = "Apache License, Version 2.0"
 __package__ = "py2neo"
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 
 
 from .neo4j import _node as node, _rel as rel
