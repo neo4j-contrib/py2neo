b801a1e581bde252b5b9c8c445b7d2d7a5aa82c8
Simon <noisyboiler@googlemail.com>
2014-02-09 15:35:29 +0000

    version marker

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import logging
 
 import logutils
@@ -6,7 +7,6 @@ from logutils.colorize import ColorizingStreamHandler
 
 from py2neo import cypher, neo4j
 
-# ToDo: possible environment constants
 DEFAULT_DB = "http://localhost:7474/db/data/"
 PY2NEO_LOGGING_LEVEL_COLOUR_MAP = {
     10: (None, 'blue', True),
@@ -40,11 +40,39 @@ def pytest_configure(config):
     logger.info('- tests running on Neo4J %s', db.neo4j_version)
 
 
+def pytest_unconfigure(config):
+    db = neo4j.GraphDatabaseService(DEFAULT_DB)
+    db.clear()
+    logger.info('cleared db after tests completed')
+
+
+def pytest_runtest_setup(item):
+    # looking for markers like '@pytest.mark.neoversion("X.XX")'
+    version_markers = item.get_marker("neoversion")
+    if version_markers:
+        db = neo4j.GraphDatabaseService(DEFAULT_DB)
+
+        required_version = Decimal(version_markers.args[0])
+
+        version_tuple = db.neo4j_version[:-1]  # ignore meta
+        actual_version = Decimal(
+            '{0}.{1}'.format(
+                version_tuple[0], ''.join(str(n) for n in version_tuple[1:])
+            )
+        )
+
+        if required_version > actual_version:
+            logger.warning(
+                '{test_name} requires neo > {version}'.format(
+                test_name=item.name,
+                version=required_version)
+            )
+
+            pytest.skip()
+
+
 @pytest.fixture
 def graph_db(request):
-    db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+    db = neo4j.GraphDatabaseService(DEFAULT_DB)
     db.clear()
     return db
-
-# ToDo: varsion marker
-
