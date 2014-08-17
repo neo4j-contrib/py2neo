ae6376fadfecaf77e16609e3c292f121f970a788
Nigel Small <nigel@nigelsmall.com>
2014-08-17 09:49:44 +0100

    update setup.py to include new packages

--- setup.py
+++ setup.py
@@ -28,61 +28,69 @@ except ImportError:
 from py2neo import __author__, __email__, __license__, __package__, __version__
 
 
-def do_setup(extensions=None):
-    setup(
-        name=__package__,
-        version=__version__,
-        description="Python client library for the Neo4j REST server",
-        long_description="Py2neo is a simple and pragmatic Python library that "
-                         "provides access to the popular graph database Neo4j via "
-                         "its RESTful web service interface. With no external "
-                         "dependencies, installation is straightforward and "
-                         "getting started with coding is easy. The library is "
-                         "actively maintained on GitHub, regularly updated in the "
-                         "Python Package Index and is built uniquely for Neo4j in "
-                         "close association with its team and community.",
-        author=__author__,
-        author_email=__email__,
-        url="http://nigelsmall.com/py2neo",
-        scripts=[
-            "scripts/neotool",
-        ],
-        packages=[
-            "py2neo",
-            "py2neo.packages",
-            "py2neo.packages.httpstream",
-            "py2neo.packages.jsonstream",
-            "py2neo.packages.urimagic",
-        ],
-        install_requires=[
-        ],
-        license=__license__,
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.3",
-            "Topic :: Database",
-            "Topic :: Software Development",
-        ],
-        ext_modules=extensions,
-        zip_safe=False,
-    )
+python_2 = sys.version_info < (3,)
 
-py3k = sys.version_info >= (3,)
-sdist = "sdist" in sys.argv
+package_metadata = {
+    "name": __package__,
+    "version": __version__,
+    "description": "Python client library and toolkit for Neo4j",
+    # TODO: rewrite long description for 2.0
+    "long_description": "Py2neo is a simple and pragmatic Python library that "
+                        "provides access to the popular graph database Neo4j via "
+                        "its RESTful web service interface. With no external "
+                        "dependencies, installation is straightforward and "
+                        "getting started with coding is easy. The library is "
+                        "actively maintained on GitHub, regularly updated in the "
+                        "Python Package Index and is built uniquely for Neo4j in "
+                        "close association with its team and community.",
+    "author": __author__,
+    "author_email": __email__,
+    "url": "http://nigelsmall.com/py2neo",
+    "scripts": [
+        "scripts/neotool",
+    ],
+    "packages": [
+        "py2neo",
+        "py2neo.batch",
+        "py2neo.cypher",
+        "py2neo.ext",
+        "py2neo.ext.admin",
+        "py2neo.ext.calendar",
+        "py2neo.ext.gremlin",
+        "py2neo.ext.ogm",
+        "py2neo.legacy",
+        "py2neo.packages",
+        "py2neo.packages.httpstream",
+        "py2neo.packages.httpstream.packages",
+        "py2neo.packages.httpstream.packages.urimagic",
+        "py2neo.packages.jsonstream",
+        "py2neo.tool",
+    ],
+    "license": __license__,
+    "classifiers": [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Database",
+        "Topic :: Software Development",
+    ],
+    "zip_safe": False,
+}
 
 extensions = []
-if sdist or py3k:
+sdist = "sdist" in sys.argv
+if sdist or not python_2:
     extensions.append(Extension("py2neo.packages.jsonstream.cjsonstream",
                                 ["py2neo/packages/jsonstream/cjsonstream.c"]))
-elif sdist or not py3k:
+if sdist or python_2:
     extensions.append(Extension("py2neo.packages.jsonstream.cjsonstream_2x",
                                 ["py2neo/packages/jsonstream/cjsonstream_2x.c"]))
 
 try:
-    do_setup(extensions)
+    setup(ext_modules=extensions, **package_metadata)
 except:
-    do_setup()
+    setup(**package_metadata)
