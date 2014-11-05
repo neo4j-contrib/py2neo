c6f9f18dc790b23652a8fe86a7f8a2b1b313f86d
Nigel Small <nigel.small@neotechnology.com>
2014-11-05 01:44:13 +0000

    URI __init__ to __new__

--- py2neo/packages/httpstream/packages/urimagic/rfc3986.py
+++ py2neo/packages/httpstream/packages/urimagic/rfc3986.py
@@ -560,42 +560,43 @@ class URI(Part):
             path = Path(value)
         return authority, path
 
-    def __init__(self, value):
-        super(URI, self).__init__()
-        if isinstance(value, URI):
-            self.__scheme = value.__scheme
-            self.__authority = value.__authority
-            self.__path = value.__path
-            self.__query = value.__query
-            self.__fragment = value.__fragment
+    __scheme = None
+    __authority = None
+    __path = None
+    __query = None
+    __fragment = None
+
+    def __new__(cls, value):
+        if isinstance(value, cls):
+            return value
+        inst = super(cls, URI).__new__(cls)
+        if value is None:
+            return inst
+        try:
+            if value.__uri__ is None:
+                return inst
+        except AttributeError:
+            pass
+        try:
+            value = ustr(value.__uri__)
+        except AttributeError:
+            value = ustr(value)
+        # scheme
+        if ":" in value:
+            inst.__scheme, _, value = value.partition(":")
+            inst.__scheme = percent_decode(inst.__scheme)
         else:
-            self.__scheme = None
-            self.__authority = None
-            self.__path = None
-            self.__query = None
-            self.__fragment = None
-            try:
-                if value.__uri__ is None:
-                    return
-            except AttributeError:
-                pass
-            if value is not None:
-                try:
-                    value = ustr(value.__uri__)
-                except AttributeError:
-                    value = ustr(value)
-                # scheme
-                if ":" in value:
-                    self.__scheme, value = value.partition(":")[0::2]
-                    self.__scheme = percent_decode(self.__scheme)
-                else:
-                    self.__scheme = None
-                # fragment
-                value, self.__fragment = self._partition_fragment(value)
-                # query
-                value, self.__query = self._partition_query(value)
-                # hierarchical part
-                self.__authority, self.__path = self._parse_hierarchical_part(value)
+            inst.__scheme = None
+        # fragment
+        value, inst.__fragment = inst._partition_fragment(value)
+        # query
+        value, inst.__query = inst._partition_query(value)
+        # hierarchical part
+        inst.__authority, inst.__path = inst._parse_hierarchical_part(value)
+        return inst
+
+    def __init__(self, value):
+        Part.__init__(self)
 
     def __hash__(self):
         return hash(self.string)
