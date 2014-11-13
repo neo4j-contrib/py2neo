1e2b002c534877c847e8ac3490e5538b98403b3d
Simon <noisyboiler@googlemail.com>
2014-11-13 12:08:24 +0000

    apache license

--- py2neo/ext/spatial/__init__.py
+++ py2neo/ext/spatial/__init__.py
@@ -1,3 +1,19 @@
-# test integration to https://github.com/neo4j-contrib/spatial
-# tests last run against 84c81475cb5d048427d2be57b4940c5a34199b7c
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2014, Simon Harrison
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from .plugin import Spatial
