8e677de07f60b17b14bb952144a9355c692f9518
Nigel Small <nigel.small@neotechnology.com>
2015-01-30 10:56:12 +0000

    Release 2.0.5

--- py2neo/__init__.py
+++ py2neo/__init__.py
@@ -21,7 +21,7 @@ __copyright__ = "2011-2014, Nigel Small"
 __email__ = "nigel@py2neo.org"
 __license__ = "Apache License, Version 2.0"
 __package__ = "py2neo"
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 
 from py2neo.core import *
