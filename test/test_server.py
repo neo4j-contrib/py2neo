5e9ec281e6a71a0bfbe221db223569aeea7d2fcf
Nigel Small <nigel.small@neotechnology.com>
2016-01-28 23:38:32 +0000

    Server tests

---
+++ test/test_server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright 2011-2015, Nigel Small
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from unittest import TestCase
+
+from py2neo.server import dist_name, dist_archive_name
+
+
+class DistFunctionTestCase(TestCase):
+
+    def test_dist_name(self):
+        name = dist_name("community", "3.0.0")
+        assert name == "neo4j-community-3.0.0"
+
+    def test_dist_archive_name(self):
+        name = dist_archive_name("community", "3.0.0")
+        assert name == "neo4j-community-3.0.0-unix.tar.gz"
