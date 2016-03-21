16a26f6797ff87c8668848b1051b57b502b79c4d
Nigel Small <nigelsmall@protonmail.com>
2016-03-21 20:53:33 +0000

    Working but complicated

--- py2neo/ogm.py
+++ py2neo/ogm.py
@@ -52,47 +52,43 @@ class Related(object):
 
     __instance__ = None
 
-    def __init__(self, type=None):
-        self.type = type
+    def __init__(self, related_node_type, relationship_type=None):
+        self.related_node_type = related_node_type
+        self.relationship_type = relationship_type
 
-    @property
-    def items(self):
-        return self.__instance__.__db_relationships__.setdefault(self.type, set())
+    def __get__(self, instance, owner):
+        return instance.__db_relationships__.setdefault(self.relationship_type, set())
 
-    def add(self, value):
-        if isinstance(value, GraphObject):
-            self.items.add(value)
-        else:
-            raise TypeError("Related items must be GraphObject instances")
+    def add(self, item):
+        pass
 
-    def remove(self, value):
-        if isinstance(value, GraphObject):
-            self.items.remove(value)
-        else:
-            raise TypeError("Related items must be GraphObject instances")
+    def remove(self, item):
+        pass
+
+
+class RelatedItems(object):
+
+    pass
 
 
 class GraphObjectMeta(type):
 
     def __new__(mcs, name, bases, attributes):
-        related = []
         for attr_name, attr in list(attributes.items()):
             if isinstance(attr, Property):
                 attributes[attr_name] = Property(attr.key or attr_name)
             elif isinstance(attr, Label):
                 attributes[attr_name] = Label(attr.name or label_case(attr_name))
             elif isinstance(attr, Related):
-                attributes[attr_name] = Related(attr.type or relationship_case(attr_name))
-                related.append(attributes[attr_name])
+                relationship_type = attr.relationship_type or relationship_case(attr_name)
+                attributes[attr_name] = Related(attr.related_node_type, relationship_type)
         attributes.setdefault("__primary_label__", name)
         attributes.setdefault("__primary_key__", "__id__")
-        instance = super().__new__(mcs, name, bases, attributes)
-        for attr in related:
-            attr.__instance__ = instance
-        return instance
+        return super().__new__(mcs, name, bases, attributes)
 
 
 class GraphObject(metaclass=GraphObjectMeta):
+    __graph__ = None
     __primary_label__ = None
     __primary_key__ = None
 
@@ -100,7 +96,8 @@ class GraphObject(metaclass=GraphObjectMeta):
     __db_relationships = None
 
     @classmethod
-    def load(cls, graph, primary_value):
+    def load(cls, primary_value):
+        graph = cls.__graph__
         # Label:key=value
         primary_key = cls.__primary_key__
         if primary_key == "__id__":
@@ -110,12 +107,43 @@ class GraphObject(metaclass=GraphObjectMeta):
             node = graph.find_one(cls.__primary_label__, primary_key, primary_value)
         if node is None:
             raise LookupError("Cannot load object")
+        return cls.wrap(node)
+
+    @classmethod
+    def wrap(cls, node):
+        graph = cls.__graph__
         inst = GraphObject()
         inst.__db_node = node
-        print(list(graph.match(node)))
-        # TODO: load relationships
+        inst.__db_relationships = {}
+        for relationship in graph.match(node):
+            related_nodes = inst.__db_relationships.setdefault(relationship.type(), set())
+            related_nodes.add(relationship.end_node())
         inst.__class__ = cls
         return inst
+
+    def __eq__(self, other):
+        try:
+            return self.__db_node__ == other.__db_node__
+        except AttributeError:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self.__db_node__)
+
+    @property
+    def __db_node__(self):
+        if self.__db_node is None:
+            self.__db_node = Node(self.__primary_label__)
+        return self.__db_node
+
+    @property
+    def __db_relationships__(self):
+        if self.__db_relationships is None:
+            self.__db_relationships = {}
+        return self.__db_relationships
 
     @property
     def __primary_value__(self):
@@ -129,18 +157,6 @@ class GraphObject(metaclass=GraphObjectMeta):
                 return None
         else:
             return node[primary_key]
-
-    @property
-    def __db_node__(self):
-        if self.__db_node is None:
-            self.__db_node = Node(self.__primary_label__)
-        return self.__db_node
-
-    @property
-    def __db_relationships__(self):
-        if self.__db_relationships is None:
-            self.__db_relationships = {}
-        return self.__db_relationships
 
     @property
     def __remote__(self):
@@ -170,23 +186,18 @@ class Person(GraphObject):
     directed = Related("Movie")
     produced = Related("Movie")
 
-    def __init__(self, name, year_of_birth):
-        self.name = name
-        self.year_of_birth = year_of_birth
-
 
 class Movie(GraphObject):
     __primary_key__ = "title"
 
     title = Property()
+    tag_line = Property(key="tagline")
     year_of_release = Property(key="released")
-
-    def __init__(self, title, year_of_release):
-        self.title = title
-        self.year_of_release = year_of_release
 
 
 def main():
-    keanu = Person("Keanu Reeves", 1968)
-    the_matrix = Movie("The Matrix", 1999)
+    from py2neo import Graph
+    GraphObject.__graph__ = Graph()
+    keanu = Person.load("Keanu Reeves")
+    the_matrix = Movie.load("The Matrix")
     keanu.acted_in.add(the_matrix)
