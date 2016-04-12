b05dc73e6677945d9c11c3b9be588481e4afee7c
Nigel Small <nigelsmall@protonmail.com>
2016-04-12 14:47:55 +0100

    Added parallel test

---
+++ test/test_parallel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright 2011-2016, Nigel Small
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from test.util import Py2neoTestCase
+
+
+class ParallelTestCase(Py2neoTestCase):
+
+    def test_can_run_cypher_while_in_transaction(self):
+        tx = self.graph.begin()
+        outer_result = tx.run("UNWIND range(1, 10) AS n RETURN n")
+        inner_result = self.graph.run("CREATE (a) RETURN a")
+        outer_result_list = list(outer_result)
+        tx.rollback()
+        created = inner_result.next[0]
+        assert outer_result_list == [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
+        assert self.graph.exists(created)
