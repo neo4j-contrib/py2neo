be86b7e463e66d4a4368bf1241987572a68e561d
chrissng <calibre@gmail.com>
2016-09-16 09:06:35 -0700

    refactor to use py2neo util module's `is_collection` function to check instead.

--- py2neo/util.py
+++ py2neo/util.py
@@ -33,11 +33,10 @@ WORD_ALL = re.compile("([a-z0-9])([A-Z])")
 
 
 def snake_case(s):
-    if isinstance(s, list) or isinstance(s, tuple) or isinstance(s, set):
+    if is_collection(s):
         s = "_".join(s)
 
     words = s.replace("_", " ").replace("-", " ").split()
-
     return "_".join(word.lower() for word in words)
 
 
