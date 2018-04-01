9dd29e65bb20cc2b83bec7d3fcbae9b020f8f0c7
Nigel Small <nigel@neo4j.com>
2018-04-01 19:05:41 +0100

    Copyright and legal script

---
+++ check_legal.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+function check_license {
+    find py2neo -name '*.py' | xargs grep -L "http://www.apache.org/licenses/LICENSE-2.0"
+}
+
+
+function check_copyright {
+    YEAR=$(date +%Y)
+    find py2neo -name '*.py' | xargs grep -L "Copyright 2011-${YEAR}"
+}
+
+
+UNLICENSED=$(check_license)
+if [ "${UNLICENSED}" ]
+then
+    echo "The following files do not contain Apache license details:"
+    echo "${UNLICENSED}"
+    exit 1
+fi
+
+UNCOPYRIGHTED=$(check_copyright)
+if [ "${UNCOPYRIGHTED}" ]
+then
+    echo "The following files do not contain an up-to-date copyright notice:"
+    echo "${UNCOPYRIGHTED}"
+    exit 1
+fi
--- py2neo/cypher/reading.py
+++ py2neo/cypher/reading.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# coding: utf-8
+# -*- encoding: utf-8 -*-
 
-# Copyright 2002-2018, Neo4j
+# Copyright 2011-2018, Nigel Small
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
