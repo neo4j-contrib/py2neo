f6e6526e7e7a6e09396210c643a42874ec68817b
Nigel Small <nigel@neo4j.com>
2018-04-10 19:11:33 +0100

    Run wider variety of tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
+NEO4J_URI=""
+
+function run
+{
+    NEO4J_URI="$1"
+    NEO4J_SECURE="$2"
+    coverage run --append --module py2neo.testing -v
+    if [ "$?" != "0" ]
+    then
+        echo "Tests failed (NEO4J_URI=${NEO4J_URI} NEO4J_SECURE=${NEO4J_SECURE})"
+        exit 1
+    fi
+}
+
 pip install --upgrade coverage coveralls -r requirements.txt
 coverage erase
-coverage run --append --module py2neo.testing -v
-if [ "$?" != "0" ]
-then
-    echo "Tests failed under Bolt"
-    exit 1
-fi
-NEO4J_URI=http://localhost:7474 coverage run --append --module py2neo.testing -v
-if [ "$?" != "0" ]
-then
-    echo "Tests failed under HTTP"
-    exit 1
-fi
+run "" ""
+run "bolt://localhost:7687" 0
+run "bolt://localhost:7687" 1
+run "http://localhost:7474" 0
+run "https://localhost:7473" 1
 coverage report
 coveralls
