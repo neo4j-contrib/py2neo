4db08032b8d8584aeac345bc3ce88f60fbccb2ba
Nigel Small <nigel@neo4j.com>
2018-04-18 07:06:10 +0100

    Improved check_install.sh

--- check_install.sh
+++ check_install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+ROOT=$(dirname $0)
 ENV=venv
 
+cd ${ROOT}
+rm -r dist
+python setup.py sdist
+
+cd ${ROOT}/dist
 pip install --upgrade virtualenv
 virtualenv ${ENV}
 source ${ENV}/bin/activate
-pip install .
+pip install py2neo-*.tar.gz
 if [ "$?" != "0" ]
 then
     exit 1
