7fd8aee475fa17ef5b4585708fe3a2911b23c221
Nigel Small <nigel@neo4j.com>
2018-05-08 13:48:57 +0100

    book->docs

--- make_docs.sh
+++ make_docs.sh
@@ -3,8 +3,8 @@
 HOME=$(dirname $0)
 
 pip install --upgrade sphinx
-make -C ${HOME}/book html
+make -C ${HOME}/docs html
 
 echo ""
-INDEX_FILE="${HOME}/book/_build/html/index.html"
+INDEX_FILE="${HOME}/docs/_build/html/index.html"
 echo "Documentation index file can be found at file://$(cd "$(dirname "${INDEX_FILE}")"; pwd)/$(basename "${INDEX_FILE}")"
