ab03600183a57b1986d690af88ec1fe6f01d7b0b
Nigel Small <nigel@neo4j.com>
2018-06-14 00:12:10 +0100

    Rich and error output

--- py2neo/cypher/kernel.py
+++ py2neo/cypher/kernel.py
@@ -28,48 +28,84 @@ except ImportError:
     warn("The Cypher kernel requires installation of the [jupyter] extra")
     raise
 
+from neo4j.exceptions import ServiceUnavailable, CypherError
+
 from py2neo.database import Graph
 from py2neo.meta import __version__
 
 
 class CypherKernel(Kernel):
+    # ?
+    language = "Cypher"
+    language_version = "Neo4j/3.4"
 
     implementation = 'cypher_kernel'
     implementation_version = __version__
-    language = "Cypher"
-    language_version = "Neo4j/3.4"
-    banner = ""
     language_info = {
-        'name': 'cypher',
-        'file_extension': '.cypher',
+        "name": "cypher",
+        "version": __version__,
         "pygments_lexer": "py2neo.cypher",
-        "pygments_style": "vim",
+        "file_extension": ".cypher",
     }
+    banner = ""
+    help_links = []
 
     def __init__(self, **kwargs):
         Kernel.__init__(self, **kwargs)
         self.graph = Graph()
         self.banner = self.graph.database.query_jmx("org.neo4j", name="Kernel")["KernelVersion"]
 
-    def do_apply(self, content, bufs, msg_id, reply_metadata):
-        pass
-
-    def do_clear(self):
-        pass
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        try:
+            table = self.graph.run(code).to_table()
+        except CypherError as error:
+            error_content = {
+                "execution_count": self.execution_count,
+                "ename": error.code,
+                "evalue": error.message,
+                "traceback": [error.code, error.message],
+            }
+            self.send_response(self.iopub_socket, "error", error_content)
+            return dict(error_content, status="error")
+        else:
+            if not silent:
+                self.send_response(self.iopub_socket, "execute_result", {
+                    "execution_count": self.execution_count,
+                    "data": {
+                        "text/plain": repr(table),
+                        "text/html": table._repr_html_(),
+                    },
+                })
+            return {
+                'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+            }
 
     def do_complete(self, code, cursor_pos):
-        pass
+        return {'matches': [],
+                'cursor_end': cursor_pos,
+                'cursor_start': cursor_pos,
+                'metadata': {},
+                'status': 'ok'}
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
-        table = self.graph.run(code).to_table()
-        if not silent:
-            stream_content = {'name': 'stdout', 'text': repr(table)}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
-        return {'status': 'ok', 'execution_count': self.execution_count,
-                'payload': [], 'user_expressions': {}}
+    def do_inspect(self, code, cursor_pos, detail_level=0):
+        return {'status': 'ok', 'data': {}, 'metadata': {}, 'found': False}
+
+    def do_history(self, hist_access_type, output, raw, session=None, start=None,
+                   stop=None, n=None, pattern=None, unique=False):
+        return {'status': 'ok', 'history': []}
+
+    def do_shutdown(self, restart):
+        return {'status': 'ok', 'restart': restart}
+
+    def do_is_complete(self, code):
+        return {'status': 'unknown',
+                }
 
 
-def install_my_kernel_spec(user=True, prefix=None):
+def install(user=True, prefix=None):
     import os
     import json
     with TemporaryDirectory() as td:
@@ -80,7 +116,6 @@ def install_my_kernel_spec(user=True, prefix=None):
                 "display_name": "Cypher",
                 "language": "cypher",
                 "pygments_lexer": "py2neo.cypher",
-                "pygments_style": "vim",
             }, f, sort_keys=True)
         # TODO: Copy resources once they're specified
 
@@ -88,15 +123,40 @@ def install_my_kernel_spec(user=True, prefix=None):
         KernelSpecManager().install_kernel_spec(td, "cypher", user=user, prefix=prefix)
 
 
+def uninstall():
+    KernelSpecManager().remove_kernel_spec("cypher")
+
+
+def list():
+    from pprint import pprint
+    pprint(KernelSpecManager().find_kernel_specs())
+
+
+def launch():
+    c = Config()
+    c.TerminalInteractiveShell.highlighting_style = "vim"
+    kwargs = {
+        "kernel_class": CypherKernel,
+        "config": c,
+    }
+    try:
+        IPKernelApp.launch_instance(**kwargs)
+    except ServiceUnavailable:
+        app = IPKernelApp.instance(**kwargs)
+        app.exit(1)
+
+
 def main():
     import sys
     args = sys.argv[1:]
     if args and args[0] == "install":
-        install_my_kernel_spec()
+        install()
+    elif args and args[0] == "uninstall":
+        uninstall()
+    elif args and args[0] == "list":
+        list()
     else:
-        c = Config()
-        c.TerminalInteractiveShell.highlighting_style = "vim"
-        IPKernelApp.launch_instance(kernel_class=CypherKernel, config=c)
+        launch()
 
 
 if __name__ == "__main__":
