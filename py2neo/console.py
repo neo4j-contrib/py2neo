756bf5a1df543877aefea8f8cbd721997a9ccdf5
Nigel Small <nigel@neo4j.com>
2020-06-18 13:54:27 +0100

    Console package to module

--- py2neo/console.py
+++ py2neo/console.py
@@ -20,7 +20,7 @@ from __future__ import division, print_function
 
 import shlex
 from datetime import datetime
-from os import makedirs
+from os import environ, makedirs
 from os.path import expanduser, join as path_join
 from subprocess import call
 from tempfile import NamedTemporaryFile
@@ -35,11 +35,69 @@ from prompt_toolkit.styles import merge_styles, style_from_pygments_cls, style_f
 from pygments.styles.native import NativeStyle
 from pygments.token import Token
 
-from py2neo.console.meta import HISTORY_FILE_DIR, HISTORY_FILE, TITLE, QUICK_HELP, EDITOR, DESCRIPTION, FULL_HELP
+from py2neo.connect import ConnectionProfile, Failure
 from py2neo.cypher.lexer import CypherLexer
 from py2neo.data import Table
 from py2neo.database import Graph
-from py2neo.connect import ConnectionProfile, Failure
+from py2neo.meta import __version__
+
+
+EDITOR = environ.get("EDITOR", "vim")
+
+HISTORY_FILE_DIR = expanduser(path_join("~", ".py2neo"))
+
+HISTORY_FILE = "console_history"
+
+TITLE = "Py2neo console v{}".format(__version__)
+
+DESCRIPTION = "Py2neo console is a Cypher runner and interactive tool for Neo4j."
+
+QUICK_HELP = """\
+  //  to enter multi-line mode (press [Alt]+[Enter] to run)
+  /e  to launch external editor
+  /?  for help
+  /x  to exit\
+"""
+
+FULL_HELP = """\
+If command line arguments are provided, these are executed in order as
+statements. If no arguments are provided, an interactive console is
+presented.
+
+Statements entered at the interactive prompt or as arguments can be
+regular Cypher, transaction control keywords or slash commands. Multiple
+Cypher statements can be entered on the same line separated by semicolons.
+These will be executed within a single transaction.
+
+For a handy Cypher reference, see:
+
+  https://neo4j.com/docs/cypher-refcard/current/
+
+Transactions can be managed interactively. To do this, use the transaction
+control keywords BEGIN, COMMIT and ROLLBACK.
+
+Slash commands provide access to supplementary functionality.
+
+\b
+{}
+
+\b
+Execution commands:
+  /play     run a query from a file
+
+\b
+Formatting commands:
+  /csv      format output as comma-separated values
+  /table    format output in a table
+  /tsv      format output as tab-separated values
+
+\b
+Information commands:
+  /config   show Neo4j server configuration
+  /kernel   show Neo4j kernel information
+
+Report bugs to py2neo@nige.tech\
+""".format(QUICK_HELP)
 
 
 def is_command(source):
