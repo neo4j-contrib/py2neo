713059dcaa5d9ecce6767b01ff22c70dfcc56552
Nigel Small <nigel@neo4j.com>
2020-07-15 07:58:13 +0100

    Requirements tweaks

--- setup.py
+++ setup.py
@@ -15,22 +15,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os import path
+from os import getenv, path
+from warnings import warn
 
 from setuptools import setup, find_packages
 
-from py2neo.meta import get_metadata, PatchedVersion
+from py2neo.meta import VERSION_FILE, get_metadata, parse_version_string
 
 
 README_FILE = path.join(path.dirname(__file__), "README.rst")
 
-with open(README_FILE) as _f:
-    README = _f.read()
+
+def get_readme():
+    with open(README_FILE) as f:
+        return f.read()
 
 
-with PatchedVersion():
+class Release(object):
+
+    def __init__(self):
+        self.__original = None
+
+    def __enter__(self):
+        self.__original = parse_version_string(self._read_version())
+        if self.__original["dev"]:
+            patched = parse_version_string(self._read_patched())
+            self._check_compatible(self.__original, patched)
+            self._patch_version(patched["string"])
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._patch_version(self.__original["string"])
+
+    @classmethod
+    def _read_version(cls):
+        with open(VERSION_FILE, "r") as f:
+            return f.read()
+
+    @classmethod
+    def _read_patched(cls):
+        patched = getenv("RELEASE")
+        if patched:
+            return patched
+        else:
+            warn("RELEASE environment variable not set - assuming development release")
+            return cls._read_version()
+
+    @classmethod
+    def _check_compatible(cls, original, patched):
+        if original == patched:
+            return
+        compatible = True
+        if patched["epoch"] != original["epoch"]:
+            compatible = False
+        if patched["release"][:2] != original["release"][:2]:
+            compatible = False
+        if patched["dev"] is not None:
+            compatible = False
+        if not compatible:
+            raise SystemExit("Patched version string %r is not compatible with original version "
+                             "string %r" % (patched["string"], original["string"]))
+
+    @classmethod
+    def _patch_version(cls, value):
+        with open(VERSION_FILE, "w") as f:
+            f.write(value)
+
+
+with Release():
     setup(**dict(get_metadata(), **{
-        "long_description": README,
+        "long_description": get_readme(),
         "long_description_content_type": "text/x-rst",
         "entry_points": {
             "console_scripts": [
@@ -47,14 +101,13 @@ with PatchedVersion():
         "py_modules": [],
         "install_requires": [
             "certifi",
-            "colorama",
             "docker",
             "monotonic",
             "neotime~=1.7.4",
             "packaging",
             "pansi>=2020.7.3",
             "prompt_toolkit~=2.0.7",
-            "pygments~=2.5.2",
+            "pygments>=2.0.0",
             "pyopenssl",
             "pytz",
             "six>=1.15.0",
