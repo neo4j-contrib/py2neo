c7169e8f827446454894e77a5b74bddc1647e638
moomoohk <moomoohk2@gmail.com>
2021-01-03 21:34:34 +0200

    Fix SystemGraph constructor

--- py2neo/database/__init__.py
+++ py2neo/database/__init__.py
@@ -730,8 +730,9 @@ class SystemGraph(Graph):
     *New in version 2020.0.*
     """
 
-    def __new__(cls, profile=None, **settings):
-        return super(SystemGraph, cls).__new__(profile, name="system", **settings)
+    def __init__(self, profile=None, **settings):
+        settings.pop("name", None)
+        super().__init__(profile, name="system", **settings)
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__.__name__, self.service.uri)
