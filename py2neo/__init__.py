fc73aa6361444ebf90d70bacac1830d4b90a9f23
Nigel Small <nasmall@pm.me>
2021-04-19 17:32:14 +0100

    Updated future imports to fix unicode issue in py27

--- py2neo/__init__.py
+++ py2neo/__init__.py
@@ -38,7 +38,7 @@ To run a query against a local database is straightforward::
 
 """
 
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, print_function
 
 
 __all__ = [
